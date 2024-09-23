b3550d93a3a156d84a5276cfb5fa73d2c2a7985e
kobe <kobecode24@outlook.com>
2024-09-23 13:45:00 +0100

    Refactor

--- src/com/baticuisine/model/Estimate.java
+++ src/com/baticuisine/model/Estimate.java
@@ -17,10 +17,6 @@ public class Estimate {
         this.accepted = false;
     }
 
-    public double calculateTotalWithMargin(double profitMargin) {
-        return estimatedAmount * (1 + profitMargin);
-    }
-
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
